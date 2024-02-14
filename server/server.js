51cfa5e019cf7aa1ed9db4ba2d4a4b49d082948b
Pranay9752 <pranayambre01@gmail.com>
2024-02-15 00:23:21 +0530

    UPDATE

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const multer  = require('multer')
+
+//import routes
+const authRoutes = require('./routes/auth');
+const projectRoutes = require('./routes/project');
+
+
+//app
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({limit:'50mb'})); 
+app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
+
+// db
+mongoose.connect(process.env.MONGODB_URI,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log(err));
+
+//middlewares
+// multer
+
+
+
+//routes middleware
+app.use('/uploads', express.static('uploads'));
+app.use('/api/user', authRoutes);
+app.use('/api/product', projectRoutes);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+});
