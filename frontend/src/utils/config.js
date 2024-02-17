2cd41baa6bd5535af407787e9c945c09990fca69
Pranay9752 <pranayambre01@gmail.com>
2024-02-17 23:18:31 +0530

    UPDTE

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -6,7 +6,7 @@
 
 // const BASE_URL='http://localhost:5000/'
 const BASE_URL = "https://taskifyr.vercel.app/"
-export const REACT_BASE_URL='http://localhost:3000/'
+export const REACT_BASE_URL='https://taskifyre.vercel.app/'
 
 function isManager(team) {
     
