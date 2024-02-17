d4860d002b968606aaa28415ff128e727543ad87
Pranay9752 <pranayambre01@gmail.com>
2024-02-17 23:13:36 +0530

    UPDTE

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const multer  = require('multer')
+const multer = require('multer')
 
 //import routes
 const authRoutes = require('./routes/auth');
@@ -12,17 +12,19 @@ const projectRoutes = require('./routes/project');
 
 //app
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://taskifyre.vercel.app/"
+}));
 
 
-app.use(bodyParser.json({limit:'50mb'})); 
-app.use(bodyParser.urlencoded({extended:true, limit:'50mb'})); 
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 // db
-mongoose.connect(process.env.MONGODB_URI,{
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-   })
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('DB Connected'))
   .catch((err) => console.log(err));
 
