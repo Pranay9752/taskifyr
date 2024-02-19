d817e309cc4cae23a165625d3fea19686aa7f42b
Pranay9752 <pranayambre01@gmail.com>
2024-02-19 16:51:06 +0530

    UPDATE

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -5,7 +5,7 @@
 
 
 // const BASE_URL='http://localhost:5000/'
-const BASE_URL = "https://taskifyr.vercel.app/"
+const BASE_URL = "https://odd-pink-vulture-wear.cyclic.app/"
 export const REACT_BASE_URL='https://taskifyre.vercel.app/'
 // export const REACT_BASE_URL='http://localhost:3000/'
 
